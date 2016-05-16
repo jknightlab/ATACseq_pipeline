0a56ce0c94a0b1d7f353669ed2531b92dffabdbf
pulyakhina <pulyakhina@gmail.com>
2016-05-16 15:40:51 +0100

    changed layout of the histograms

--- Core_manuscript/Fseq_Macs2_Hotspot/create_heatmap.R
+++ Core_manuscript/Fseq_Macs2_Hotspot/create_heatmap.R
@@ -16,3 +16,25 @@ pdf(output_name)
 heatmap.2(nba_matrix, dendrogram='none', Rowv=FALSE, Colv=FALSE,trace='none', density.info="none", main=title, tracecol=NA, col=my_palette, labRow=rownames(nba_matrix), margins=c(10, 10), cexRow=2, cexCol=2, keysize = 1)
 dev.off()
 
+
+
+heatmap.2(nba_matrix, Rowv=FALSE,Colv=FALSE, 
+          col = my_palette,
+          scale="none",
+          margins=c(10, 4),
+          trace='none', 
+          symkey=FALSE, 
+          symbreaks=FALSE, 
+          dendrogram='none',
+          cexRow=2,
+          cexCol=2,
+          density.info='none', 
+          denscol=tracecol,
+          key=2,
+          key.xlab = "",
+          key.title = "",
+          key.xtickfun = NULL,
+          key.par=list(mar=c(3.5,0,3,0)),
+          lmat=rbind(c(5, 4, 2), c(6, 1, 3)), lhei=c(1.5, 5), lwid=c(1, 10, 1))
+
+
