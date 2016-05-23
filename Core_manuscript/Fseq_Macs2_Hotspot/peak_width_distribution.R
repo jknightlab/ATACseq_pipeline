93938af933b59b415b45be61c8beecfb158a680d
pulyakhina <pulyakhina@gmail.com>
2016-05-23 13:21:59 +0100

    peak width distribution

--- Core_manuscript/Fseq_Macs2_Hotspot/peak_width_distribution.R
+++ Core_manuscript/Fseq_Macs2_Hotspot/peak_width_distribution.R
@@ -8,17 +8,19 @@ f_cho <- density(as.numeric(fseq_chosen[,3]-fseq_chosen[,2]))
 macs_def <- density(as.numeric(macs_default[,3]-macs_default[,2]))
 macs_cho <- density(as.numeric(macs_chosen[,3]-macs_chosen[,2]))
 
-plot(f_cho, main="Peak width distribution", cex.axis=2, cex.lab=2, xlab="", ylab="", cex.main=3)
+plot(macs_cho, main="Peak width distribution", cex.axis=2, cex.lab=2, xlab="", ylab="", cex.main=3)
 
 polygon(f_def, border="black", col=adjustcolor("deepskyblue",alpha.f=0.2))
 polygon(f_cho, border="black", col=adjustcolor("darkblue",alpha.f=0.2))
 polygon(macs_def, border="black", col=adjustcolor("chartreuse3",alpha.f=0.2))
 polygon(macs_cho, border="black", col=adjustcolor("darkgreen",alpha.f=0.2))
 
-legend(1100, 0.0055, c("FSeq default parameters", "FSeq chosen parameters", "Macs2 default parameters", "Macs2 chosen parameters"), lty=c(1,1), lwd=c(4.5,4.5),col=c("deepskyblue", "darkblue", "chartreuse3", "darkgreen"), cex=2)
+legend(1900, 0.0032, c("FSeq default", "FSeq chosen", "Macs2 default", "Macs2 chosen"), lwd=c(3,3), col=c("deepskyblue", "darkblue", "chartreuse3", "darkgreen"), cex=1.5)
 
 
-plot(f_cho, main="Peak width distribution", cex.axis=2, cex.lab=2, xlab="", ylab="", cex.main=3, xlim=c(0, 500))
+
+
+plot(macs_cho, main="Peak width distribution", cex.axis=2, cex.lab=2, xlab="", ylab="", cex.main=3, xlim=c(0, 1000))
 
 polygon(f_def, border="black", col=adjustcolor("deepskyblue",alpha.f=0.2))
 # abline(v=median(as.numeric(fseq_fedault[,3]-fseq_fedault[,2])), col="deepskyblue", lwd=3)
@@ -32,5 +34,5 @@ polygon(macs_def, border="black", col=adjustcolor("chartreuse3",alpha.f=0.2))
 polygon(macs_cho, border="black", col=adjustcolor("darkgreen",alpha.f=0.2))
 # abline(v=median(as.numeric(macs_chosen[,3]-macs_chosen[,2])), col="darkgreen", lwd=3)
 
-legend(300, 0.0055, c("FSeq default parameters", "FSeq chosen parameters", "Macs2 default parameters", "Macs2 chosen parameters"), lty=c(1,1), lwd=c(4.5,4.5),col=c("deepskyblue", "darkblue", "chartreuse3", "darkgreen"), cex=2)
+legend(750, 0.0032, c("FSeq default", "FSeq chosen", "Macs2 default", "Macs2 chosen"), lwd=c(3,3),col=c("deepskyblue", "darkblue", "chartreuse3", "darkgreen"), cex=1.5)
 
